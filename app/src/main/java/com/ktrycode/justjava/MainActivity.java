d3dc53c70ed88b79dae1832da4ffc5da5c6017f5
KTryCode <32237739+KTryCode@users.noreply.github.com>
2018-02-04 21:32:29 +0100

    1.0
    Final version, summarise of Udacity Scholarship Course

--- app/src/main/java/com/ktrycode/justjava/MainActivity.java
+++ app/src/main/java/com/ktrycode/justjava/MainActivity.java
@@ -1,12 +1,20 @@
 package com.ktrycode.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+
+import static java.net.Proxy.Type.HTTP;
 
 /**
  * This app displays an order form to order coffee.
@@ -19,32 +27,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private int quantity =0;
-    private int price=0;
-    private final int cupCost=5;
-    String nameOfCustomer="Krystian";
+    private int quantity = 1;
+    private int price = 0;
+    private final int cupCost = 5;
+    private final int chocolate = 2;
+    private final int cream = 1;
 
-    public void incrementNumberOfCoffees(View view){
+    public void incrementNumberOfCoffees(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot order more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity++;
         displayQuantity(quantity);
+
     }
 
-    public void decrementNumberOfCoffees(View view){
-        if (quantity>0) {
-            quantity--;
-            displayQuantity(quantity);
+    public void decrementNumberOfCoffees(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
         }
+        quantity--;
+        displayQuantity(quantity);
+
     }
 
     public void submitOrder(View view) {
-        if (quantity>0) {
-            price=  calculatePrice();
-            displayMessage(createOrderSummary(price));
-        } else{
+ /*       if (quantity > 0) {
+            CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+            boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+            Log.v("MainActivity", "Has whipped cream? -> " + hasWhippedCream);
+
+            CheckBox chocolateToppingCheckBox = (CheckBox) findViewById(R.id.chocolate_topping_checkbox);
+            boolean hasChocolate = chocolateToppingCheckBox.isChecked();
+            Log.v("MainActivity", "Has chocolate? ->  " + hasChocolate);
+
+            EditText nameOfCustomerEditTextView = (EditText) findViewById(R.id.name_of_customer_edit_text_view);
+            String customerName = nameOfCustomerEditTextView.getText().toString();
+
+            price = calculatePrice(hasWhippedCream, hasChocolate);
+            displayMessage(createOrderSummary(customerName, price, hasWhippedCream, hasChocolate));
+        } else {
             String priceMessage = "Free!";
             displayMessage(priceMessage);
-        }
+        }*/
 
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateToppingCheckBox = (CheckBox) findViewById(R.id.chocolate_topping_checkbox);
+        boolean hasChocolate = chocolateToppingCheckBox.isChecked();
+
+        TextView customerNameTextView = (TextView) findViewById(R.id.name_of_customer_edit_text_view);
+        String customerName = customerNameTextView.getText().toString();
+
+        price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        String emailSubject = "Order summary for " + customerName;
+        String messageBody = createOrderSummary(customerName, price, hasWhippedCream, hasChocolate);
+        composeEmail(emailSubject, messageBody);
+
+    }
+
+    public void composeEmail(String subject, String orderSummary) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private void displayQuantity(int number) {
@@ -60,23 +113,38 @@ public class MainActivity extends AppCompatActivity {
         return NumberFormat.getCurrencyInstance(Locale.US).format(number);
     }
 
-    private void displayMessage(String text){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(text);
-    }
-    private void calculatePrice(int quantity1,int costPerCup){
-        this.price=quantity1*costPerCup;
-    }
-    private int calculatePrice(){
-        return quantity*cupCost;
-    }
-    private void calculatePrice(int quantity1){
-        this.price=quantity1*10;
+//    private void displayMessage(String text) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(text);
+//    }
+
+    private void calculatePrice(int quantity1, int costPerCup) {
+        this.price = quantity1 * costPerCup;
     }
 
-    private String createOrderSummary(int cost){
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolateTopping) {
+        int totalCost = 0;
+        totalCost += cupCost;
+        if (addWhippedCream) {
+            totalCost += cream;
+        }
+        if (addChocolateTopping) {
+            totalCost += chocolate;
+        }
+
+        return quantity * totalCost;
+    }
+
+    private void calculatePrice(int quantity1) {
+        this.price = quantity1 * 10;
+    }
+
+
+    private String createOrderSummary(String customerName, int cost, boolean addWhippedCream, boolean addChocolateTopping) {
         String message;
-        message = "Name: " + nameOfCustomer +
+        message = "Name: " + customerName +
+                "\nAdd whipped cream? " + String.valueOf(addWhippedCream) +
+                "\nAdd chocolate? " + String.valueOf(addChocolateTopping) +
                 "\nQuantity: " + quantity +
                 "\nTotal: " + displayPrice(cost) +
                 "\nThank you!";
